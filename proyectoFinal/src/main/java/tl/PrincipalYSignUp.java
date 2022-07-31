31d41715afbb413bd83218fe671b8f430cb561d3
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-07-31 11:15:23 -0600

    Agregando los atributos de la clase PrincipalYSignUp.fxml

---
+++ proyectoFinal/src/main/java/tl/PrincipalYSignUp.java
@@ -0,0 +1,31 @@
+package tl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class PrincipalYSignUp {
+    /**
+     * Atributos clase PrincipalYSignUp
+     */
+    @FXML
+    private Button button_login;
+    @FXML
+    private Button button_signUp;
+    @FXML
+    private TextField tf_userName;
+    @FXML
+    private PasswordField tf_userPassword;
+
+
+
+}
