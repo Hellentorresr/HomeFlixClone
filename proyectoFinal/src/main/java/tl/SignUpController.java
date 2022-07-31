3647df59346eebd124d5cbdb6beab120a2014cb8
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-07-31 11:03:33 -0600

    Agregando atributos de la clase SingUpController

---
+++ proyectoFinal/src/main/java/tl/SignUpController.java
@@ -0,0 +1,26 @@
+package tl;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class SignUpController {
+    /**
+     * Atributos
+     */
+    public ImageView imageView;
+    private String image;
+    public Button btnSignUp;
+    public Button btnCleanSignUp;
+    public Button btnOpenFile;
+    public Button regresar;
+
+    //registro
+    public TextField txtNombreSignUp;
+    public TextField txtApellidoSignUp;
+    public TextField txtIdSignUp;
+    public TextField txtNombreUsuarioSignUp;
+    public PasswordField tf_password;
+    public PasswordField tf_passwordConfirm;
+}
