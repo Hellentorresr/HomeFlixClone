65be6aa9732931e9c67882ec0151d636ad044218
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-07-31 11:48:25 -0600

    Se crea el metodo initialize

--- proyectoFinal/src/main/java/tl/ReproductorVideoController.java
+++ proyectoFinal/src/main/java/tl/ReproductorVideoController.java
@@ -1,15 +1,25 @@
 package tl;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -19,11 +29,14 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import view.Main;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.concurrent.Callable;
 
-public class ReproductorVideoController {
+public class ReproductorVideoController implements Initializable {
     /**
      * Atributos de la clase ReproductorVideoController
      */
@@ -80,6 +93,239 @@ public class ReproductorVideoController {
     private ImageView ivExit;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {//paquete de recursos=resource bundle
+        final int IV_SIZE = 25;
+        for (int i = 0; i < Administracion.videos.size(); i++) {
+            mediaVideo = new Media(new File(Administracion.videos.get(i).getVideoPath()).toURI().toString());
+        }
+
+        //this is the video that I'm going to be playing
+        // mediaVideo = new Media(new File("src/media/JessicaDarrow.mp4").toURI().toString());
+        //with this object I can call methods like play,stop, pause
+        //the mediaPlayer wraps the media object
+        mpVideo = new MediaPlayer(mediaVideo);
+        //this displays the media that we want to display
+        mvVideo.setMediaPlayer(mpVideo);
+
+        //To add images
+        Image imagePlay = new Image(new File("src/media/play.png").toURI().toString());
+        ivPlay = new ImageView(imagePlay);
+        ivPlay.setFitHeight(IV_SIZE);
+        ivPlay.setFitWidth(IV_SIZE);
+
+        Image imageStop = new Image(new File("src/media/stop.png").toURI().toString());
+        ivPause = new ImageView(imageStop);
+        ivPause.setFitHeight(IV_SIZE);
+        ivPause.setFitWidth(IV_SIZE);
+
+        Image imageRestart = new Image(new File("src/media/restart.png").toURI().toString());
+        ivRestart = new ImageView(imageRestart);
+        ivRestart.setFitHeight(IV_SIZE);
+        ivRestart.setFitWidth(IV_SIZE);
+
+        Image imageVolume = new Image(new File("src/media/volume.png").toURI().toString());
+        ivVolume = new ImageView(imageVolume);
+        ivVolume.setFitHeight(IV_SIZE);
+        ivVolume.setFitWidth(IV_SIZE);
+
+        Image imageFullScreen = new Image(new File("src/media/fullScreen.png").toURI().toString());
+        ivFullScreen = new ImageView(imageFullScreen);
+        ivFullScreen.setFitHeight(IV_SIZE);
+        ivFullScreen.setFitWidth(IV_SIZE);
+
+        Image imageMute = new Image(new File("src/media/mute.png").toURI().toString());
+        ivMute = new ImageView(imageMute);
+        ivMute.setFitHeight(IV_SIZE);
+        ivMute.setFitWidth(IV_SIZE);
+
+        Image imageExit = new Image(new File("src/media/exit.png").toURI().toString());
+        ivExit = new ImageView(imageExit);
+        ivExit.setFitHeight(IV_SIZE);
+        ivExit.setFitWidth(IV_SIZE);
+
+        //
+        buttonPPR.setGraphic(ivPlay);//here I fixed
+        labelVolume.setGraphic(ivMute);
+        labelSpeed.setText("1x");
+        labelFullScreen.setGraphic(ivFullScreen);
+
+        //Start using the button or adding functionality
+        buttonPPR.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                bindCurrentTimeLabel();
+                Button buttonPlay = (Button) actionEvent.getSource();
+                if (atEndOfVideo) {
+                    sliderTime.setValue(0);
+                    atEndOfVideo = false;
+                    isPlaying = false;
+                }
+                if (isPlaying) {
+                    buttonPlay.setGraphic(ivPlay);
+                    mpVideo.pause();
+                    isPlaying = false;
+                } else {
+                    buttonPlay.setGraphic(ivPause);
+                    mpVideo.play();
+                    isPlaying = true;
+                }
+            }
+        });
+
+
+        //appear disappear button
+        hboxVolume.getChildren().remove(sliderVolume);
+        mpVideo.volumeProperty().bindBidirectional(sliderVolume.valueProperty());
+
+        bindCurrentTimeLabel();
+
+        sliderVolume.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                mpVideo.setVolume(sliderVolume.getValue());
+                if (mpVideo.getVolume() != 0.0) {
+                    labelVolume.setGraphic(ivVolume);
+                    isMuted = false;
+                } else {
+                    labelVolume.setGraphic(ivMute);
+                    isMuted = true;
+                }
+            }
+        });
+
+        labelSpeed.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (labelSpeed.getText().equals("1x")) {
+                    labelSpeed.setText("2x");
+                    mpVideo.setRate(2.0);
+                } else {
+                    labelSpeed.setText("1x");
+                    mpVideo.setRate(1.0);
+                }
+            }
+        });
+
+        labelVolume.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (isMuted) {
+                    labelVolume.setGraphic(ivVolume);
+                    sliderVolume.setValue(0.2);
+                    isMuted = false;
+                } else {
+                    labelVolume.setGraphic(ivMute);
+                    sliderVolume.setValue(0);
+                    isMuted = true;
+                }
+            }
+        });
+
+        labelVolume.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (hboxVolume.lookup("#sliderVolume") == null) {
+                    hboxVolume.getChildren().add(sliderVolume);
+                    sliderVolume.setValue(mpVideo.getVolume());
+                }
+            }
+        });
+
+        hboxVolume.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                hboxVolume.getChildren().remove(sliderVolume);
+            }
+        });
+
+        vboxParent.sceneProperty().addListener(new ChangeListener<Scene>() {
+            @Override
+            public void changed(ObservableValue<? extends Scene> observableValue, Scene oldScene, Scene newScene) {
+                mvVideo.fitHeightProperty().bind(newScene.heightProperty().subtract(hboxControls.heightProperty().add(20)));
+            }
+        });
+
+        labelFullScreen.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Label label = (Label) mouseEvent.getSource();
+                Stage stage = (Stage) label.getScene().getWindow();
+                if (stage.isFullScreen()) {
+                    stage.setFullScreen(false);
+                    labelFullScreen.setGraphic(ivFullScreen);
+                } else {
+                    stage.setFullScreen(true);
+                    labelFullScreen.setGraphic(ivExit);
+                }
+
+                stage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent keyEvent) {
+                        if (keyEvent.getCode() == KeyCode.ESCAPE) {
+                            labelFullScreen.setGraphic(ivFullScreen);
+                        }
+                    }
+                });
+            }
+        });
+
+        mpVideo.totalDurationProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observableValue, Duration oldDuration, Duration newDuration) {
+                bindCurrentTimeLabel();
+                sliderTime.setMax(newDuration.toSeconds());
+                labelTotalTime.setText(getTime(newDuration));
+            }
+        });
+
+        //Time slider I want to add a listener to the value changing property
+        sliderTime.valueChangingProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean wasChanging, Boolean isChanging) {
+                //what I want to do is once the slider has stopped changing meaning the user has let go of the slider ball,
+                //so I have to set the video to that time
+                bindCurrentTimeLabel();
+                if (!isChanging) {//if this is no longer true
+                    mpVideo.seek(Duration.seconds(sliderTime.getValue()));
+                }
+            }
+        });
+
+        sliderTime.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                bindCurrentTimeLabel();
+                double currentTime = mpVideo.getCurrentTime().toSeconds();
+                if (Math.abs(currentTime - newValue.doubleValue()) > 0.5) {// half second
+                    mpVideo.seek(Duration.seconds(newValue.doubleValue()));
+                }
+                labelMatchEndVideo(labelCurrentTime.getText(), labelTotalTime.getText());
+            }
+        });
+
+        mpVideo.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observableValue, Duration oldTime, Duration newTime) {
+                bindCurrentTimeLabel();
+                if (!sliderTime.isValueChanging()) {
+                    sliderTime.setValue(newTime.toSeconds());
+                }
+                labelMatchEndVideo(labelCurrentTime.getText(), labelTotalTime.getText());
+            }
+        });
+
+        mpVideo.setOnEndOfMedia(new Runnable() {
+            @Override
+            public void run() {
+                buttonPPR.setGraphic(ivRestart);
+                atEndOfVideo = true;
+                if (!labelCurrentTime.textProperty().equals(labelTotalTime.textProperty())) {
+                    labelCurrentTime.setText(getTime(mpVideo.getTotalDuration()) + " / ");
+                }
+            }
+        });
+    }
     public void bindCurrentTimeLabel() {
         labelCurrentTime.textProperty().bind(Bindings.createStringBinding(new Callable<String>() {
             @Override
