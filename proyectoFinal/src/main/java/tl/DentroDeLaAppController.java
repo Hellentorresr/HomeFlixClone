daaf376d814ee40ff47c07f8d65eba0e2e314310
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-07-31 11:55:32 -0600

    Renombrando la clase DentroDeLaAppController

--- proyectoFinal/src/main/java/tl/DentroDeLaAppController.java
+++ proyectoFinal/src/main/java/tl/DentroDeLaAppController.java
@@ -10,7 +10,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class MainController implements Initializable {
+public class DentroDeLaAppController implements Initializable {
     @FXML
     public Button btnCerrar;
     @FXML
