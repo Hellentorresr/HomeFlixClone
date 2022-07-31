987b910c3e25c1fba5fa4e7b35b926286ba9b178
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-07-31 11:56:21 -0600

    Creando el metodo handleBtnIngresa

--- proyectoFinal/src/main/java/tl/DentroDeLaAppController.java
+++ proyectoFinal/src/main/java/tl/DentroDeLaAppController.java
@@ -2,11 +2,18 @@ package tl;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import view.Main;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 
@@ -24,5 +31,14 @@ public class DentroDeLaAppController implements Initializable {
 
     public void handleBtnIngresar(ActionEvent event) {
 
+    }
+
+
+
+    //--Methods---
+    public void handleBtnIngresar() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("PrincipalYSignInForm.fxml")));
+        Stage window = (Stage) btnCerrar.getScene().getWindow();
+        window.setScene(new Scene(root));
     }
 }
