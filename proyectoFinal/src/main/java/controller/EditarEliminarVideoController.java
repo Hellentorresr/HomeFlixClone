a787c1bd1c9a17a0b4830e100a5e74f8bd923b3e
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-05 09:03:35 -0600

    Se agrega Metodo para regresar a la página principal

--- proyectoFinal/src/main/java/controller/EditarEliminarVideoController.java
+++ proyectoFinal/src/main/java/controller/EditarEliminarVideoController.java
@@ -1,10 +1,19 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import view.Main;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class EditarEliminarVideoController {
     /**
@@ -38,4 +47,12 @@ public class EditarEliminarVideoController {
     private String image = "";
 
 
+    /**
+     *Metodo para regresar a la página principal
+     */
+    public void regresarAPrincipal(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("DentroDeLaApp.fxml")));
+        Stage window = (Stage) btnRegresar.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
 }
