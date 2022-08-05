d0f8f9bbb5ce3dfd67f269b10ed0d3f084dd1a49
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-04 20:20:58 -0600

    Se actualiza interfaz DentroDeLaApp y la controlador DentroDeLaAppController

--- proyectoFinal/src/main/java/controller/DentroDeLaAppController.java
+++ proyectoFinal/src/main/java/controller/DentroDeLaAppController.java
@@ -1,13 +1,17 @@
 package controller;
 
+import controller.dao.VideoDAO;
+import controller.dao.VideoDAOImplement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,30 +20,46 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import model.Video;
 import view.Main;
+
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-
 public class DentroDeLaAppController implements Initializable {
+    //listas
+    public static List<Video> recientesPlayed = new ArrayList<>();
+    public static String test;
+    public static Button button2 = new Button();
+    //
+    private static ArrayList<Video> videosBaseDatos = new ArrayList<>();
     @FXML
     public Button btnCerrar;
     @FXML
     public HBox recentlyPlayedContainer;
-    List<Video> recientesPlayed;
+    @FXML
+    public Button agregarVideo;
+    public Button eliminarEditarVideo;
+    ArrayList<Video> favoritas;
     @FXML
     private HBox favoritasContainer;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<Video> recientesPlayed;
-        recientesPlayed = new ArrayList<>(Administracion.videos);
-
+        VideoDAO videoDAO = new VideoDAOImplement();
+        try {
+            videosBaseDatos = new ArrayList<>(videoDAO.getALL());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        recientesPlayed = new ArrayList<>(videosBaseDatos);
         int i;
         for (i = 0; i < recientesPlayed.size(); i++) {
             ImageView img = new ImageView();
@@ -54,42 +74,57 @@ public class DentroDeLaAppController implements Initializable {
             nombre.setFont(Font.font(16));
             nombre.setTextFill(Paint.valueOf("#fff"));
 
-            Button button1 = new Button(recientesPlayed.get(i).getNombreVideo());
 
-            button1.setTextFill(Paint.valueOf("Red"));
-            button1.setCursor(Cursor.cursor("hand"));
+            button2 = new Button(recientesPlayed.get(i).getVideoPath());
+            button2.setTextFill(Paint.valueOf("Red"));
+            button2.setCursor(Cursor.cursor("hand"));
 
-            button1.setOnAction(event -> {
-                Parent root = null;
+            int finalI = i;
+            button2.setOnAction(event -> {
+                test = recientesPlayed.get(finalI).getVideoPath();
+                Parent root;
                 try {
                     root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("ReproductorVideo.fxml")));
+
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                Stage window = (Stage) button1.getScene().getWindow();
+                Stage window = (Stage) button2.getScene().getWindow();
                 window.setScene(new Scene(root));
             });
-            button1.setText("Reproducir");
-
-            vBox.getChildren().add(button1);
-
+            button2.setText("Reproducir");
+            vBox.getChildren().add(button2);
             recentlyPlayedContainer.getChildren().add(vBox);
         }
-
     }
-
-
-    public void handleBtnIngresar(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("PrincipalYSignIn.fxml")));
-        Stage window = (Stage) btnCerrar.getScene().getWindow();
-        window.setScene(new Scene(root));
-    }
-
-
 
     //--Methods---
     public void handleBtnIngresar() throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("PrincipalYSignInForm.fxml")));
+        Stage window = (Stage) btnCerrar.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    public void agregarVideo(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("RegistroVideo.fxml")));
+        Stage window = (Stage) btnCerrar.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    @Override
+    public String toString() {
+        return "DentroDeLaApp{" +
+                ", button2=" + button2 +
+                ", btnCerrar=" + btnCerrar +
+                ", recentlyPlayedContainer=" + recentlyPlayedContainer +
+                ", agregarVideo=" + agregarVideo +
+                ", favoritas=" + favoritas +
+                ", favoritasContainer=" + favoritasContainer +
+                '}';
+    }
+
+    public void eliminarEditarVideo(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("EditarEliminarVideo.fxml")));
         Stage window = (Stage) btnCerrar.getScene().getWindow();
         window.setScene(new Scene(root));
     }
