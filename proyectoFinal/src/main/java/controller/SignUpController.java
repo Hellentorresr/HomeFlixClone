e06954cb15e13032436983fc187a3db4631d0d46
Diego Huertas <dhuertasg@ucenfotec.ac.cr>
2022-08-05 17:33:27 -0600

    Se agrega la funcionalidad para que el usuario se pueda registrar. Falta conectar con DAO

--- proyectoFinal/src/main/java/controller/SignUpController.java
+++ proyectoFinal/src/main/java/controller/SignUpController.java
@@ -5,19 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.Usuario;
 import view.Main;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Objects;
 
 public class SignUpController {
+    Usuario usuario;
     /**
      * Atributos
      */
@@ -35,7 +41,50 @@ public class SignUpController {
     public TextField txtNombreUsuarioSignUp;
     public PasswordField tf_password;
     public PasswordField tf_passwordConfirm;
+    public Label labelRegister;
 
+    public SignUpController(){
+        usuario = new Usuario("","","","",0,"");
+        labelRegister = new Label();
+    }
+
+    public void registering() throws SQLException, ClassNotFoundException {
+        usuario.setNombre(txtNombreSignUp.getText());
+        String fName = usuario.getNombre();
+
+        usuario.setApellido1(txtApellidoSignUp.getText());
+        String lName1 = usuario.getApellido1();
+
+        usuario.setUserName(txtNombreUsuarioSignUp.getText());
+        String nickName = usuario.getUserName();
+
+        usuario.setUserPassword(tf_password.getText());
+        String password = usuario.getUserPassword();
+
+        usuario.setUserId(Integer.parseInt(txtIdSignUp.getText()));
+        int id = usuario.getUserId();
+        if(fName.isEmpty() || lName1.isEmpty() || nickName.isEmpty() || password.isEmpty()){
+            labelRegister.setText("Rellene los campos vacios");
+            System.out.println("No");
+        } else if (!confirmPassword()) {
+            labelRegister.setText("Las contraseñas son diferentes");
+            System.out.println("casi");
+        }else{
+            labelRegister.setText("Usuario Registrado!");
+            System.out.println("si");
+        }
+
+    }
+
+    public Boolean confirmPassword(){
+        if(tf_password.getText().equals(tf_passwordConfirm.getText())){
+            System.out.println("entra");
+            return true;
+        }else{
+            System.out.println("no entra");
+            return false;
+        }
+    }
 
     public void handleBtnOpenFile(ActionEvent event) {
         final FileChooser fileChooser = new FileChooser();
--- proyectoFinal/src/main/java/model/Usuario.java
+++ proyectoFinal/src/main/java/model/Usuario.java
@@ -5,16 +5,14 @@ import java.util.Objects;
 public class Usuario {
     private String nombre;
     private String apellido1;
-    private String apellido2;
     private String userName;
     private String userPassword;
     private int userId;
     private String img;
 
-    public Usuario(String nombre, String apellido1, String apellido2, String userName, String userPassword, int userId, String img) {
+    public Usuario(String nombre, String apellido1,  String userName, String userPassword, int userId, String img) {
         this.nombre = nombre;
         this.apellido1 = apellido1;
-        this.apellido2 = apellido2;
         this.userName = userName;
         this.userPassword = userPassword;
         this.userId = userId;
@@ -35,14 +33,6 @@ public class Usuario {
 
     public void setApellido1(String apellido1) {
         this.apellido1 = apellido1;
-    }
-
-    public String getApellido2() {
-        return apellido2;
-    }
-
-    public void setApellido2(String apellido2) {
-        this.apellido2 = apellido2;
     }
 
     public String getUserName() {
@@ -82,7 +72,7 @@ public class Usuario {
         if (this == o) return true;
         if (!(o instanceof Usuario)) return false;
         Usuario usuario = (Usuario) o;
-        return userId == usuario.userId && Objects.equals(nombre, usuario.nombre) && Objects.equals(apellido1, usuario.apellido1) && Objects.equals(apellido2, usuario.apellido2) && Objects.equals(userName, usuario.userName) && Objects.equals(userPassword, usuario.userPassword) && Objects.equals(img, usuario.img);
+        return userId == usuario.userId && Objects.equals(nombre, usuario.nombre) && Objects.equals(apellido1, usuario.apellido1) && Objects.equals(userName, usuario.userName) && Objects.equals(userPassword, usuario.userPassword) && Objects.equals(img, usuario.img);
     }
 
     @Override
@@ -90,7 +80,6 @@ public class Usuario {
         return "Usuario{" +
                 "nombre='" + nombre + '\'' +
                 ", apellido1='" + apellido1 + '\'' +
-                ", apellido2='" + apellido2 + '\'' +
                 ", userName='" + userName + '\'' +
                 ", userPassword='" + userPassword + '\'' +
                 ", userId=" + userId +
