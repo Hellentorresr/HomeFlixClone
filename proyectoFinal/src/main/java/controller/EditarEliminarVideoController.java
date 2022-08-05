41f345a9041b7ee32cf6030c902cd82f389314fd
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-05 08:58:01 -0600

    Se crea la clase EditarEliminarVideoController

---
+++ proyectoFinal/src/main/java/controller/EditarEliminarVideoController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class EditarEliminarVideoController {
+    /**
+     * Atributos clase EditarEliminarVideoController
+     */
+    public Button btnRegresar;
+    @FXML
+    private Button btnConver;
+    @FXML
+    private Button btnEliminarVideo;
+    @FXML
+    private Button btnEvitar;
+    @FXML
+    private Button btnMostrarReporte;
+    @FXML
+    private Button btnPathVideo;
+    @FXML
+    private ImageView imageViewCover;
+    @FXML
+    private TextArea textAreaReporte;
+    @FXML
+    private TextField txtCategoria;
+    @FXML
+    private TextField txtCodigoParaEditar;
+    @FXML
+    private TextField txtCodigoParaEliminar;
+    @FXML
+    private TextField txtDescription;
+    @FXML
+    private TextField txtNombreVideo;
+    private String image = "";
+
+
+}
