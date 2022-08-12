20bd819255115f740783527cfa7c815a101526a1
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-12 10:16:25 -0600

    Se implementa el metodo Metodo pathInterfazGrafica

--- proyectoFinal/src/main/java/controller/controllerApp/UtilitiesImplements.java
+++ proyectoFinal/src/main/java/controller/controllerApp/UtilitiesImplements.java
@@ -7,9 +7,15 @@
  */
 package controller.controllerApp;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import view.InicioApp;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Clase UtilitiesImplements, para implementar los metodos abstractos de la clase UtilitiesAbstract
@@ -25,8 +31,17 @@ public class UtilitiesImplements extends UtilitiesAbstract {
         return null;
     }
 
+    /**
+     * Metodo pathInterfazGrafica
+     * @param path recibe un String por parametro que será la el path de la interfaz fxml
+     * @param button recibe un Button por parametro que será la el boton que ejecutara este metodo
+     * @throws IOException generará un error si no encuentra el path de la interfaz
+     */
     @Override
     public String pathInterfazGrafica(String path, Button button) throws IOException {
-        return null;
+        Parent root = FXMLLoader.load(Objects.requireNonNull(InicioApp.class.getResource(path)));
+        Stage window = (Stage) button.getScene().getWindow();
+        window.setScene(new Scene(root));
+        return path;
     }
 }
