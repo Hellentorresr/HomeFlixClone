f0b0b110707f01c71b237d380d24ad13117a00b3
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-12 12:55:33 -0600

    Creacion de la clase PlaylistVideos con sus atributos

---
+++ proyectoFinal/src/main/java/model/PlaylistVideos.java
@@ -0,0 +1,22 @@
+/**
+ * @autor por Hellen Torres
+ * @FechaCreacion 12/08/2022
+ * @Ultima_Modificacion 12//08/2022
+ * @por Hellen torres
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ * Clase PlaylistVideos
+ */
+public class PlaylistVideos {
+    /**
+     * Atributos de la clase Playlist
+     */
+    private String namePlaylist;
+    private float totalPlayListDurationTime;
+    private String tema;
+    private LocalDate creationDate;
+}
