cf9eafeb1dafc0065fab3313cd951ed9ea8736e3
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-12 09:56:27 -0600

    Creacion de la clase UtilitiesAbstract y sus atributos

---
+++ proyectoFinal/src/main/java/controller/controllerApp/UtilitiesAbstract.java
@@ -0,0 +1,21 @@
+/**
+ * Paquete controlador que controla la interfaz
+ *  @autor por Hellen Torres
+ *  @FechaCreacion 12/08/2022
+ *  @Ultima_Modificacion 12/08/2022
+ *  @por Hellen torres
+ */
+package controller.controllerApp;
+
+/**
+ * clase UtilitiesAbstract para abstraer código repetitivo
+ */
+public class UtilitiesAbstract {
+    /**
+     * Atributos de la clase UtilitiesAbstract
+     */
+    //quiero usar un metodo que valide los numeros y no se caiga el sistema
+    //variable String para recibir path
+    private int num;
+    private String pathInterfazGrafica;
+}
