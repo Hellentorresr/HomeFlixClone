5023449f64240da6395843364bf0178038316462
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-12 09:31:46 -0600

    Se crea metodo para iniciar el servidor java

--- proyectoFinal/src/main/java/controller/server/ServerJava.java
+++ proyectoFinal/src/main/java/controller/server/ServerJava.java
@@ -16,5 +16,31 @@ public class ServerJava {
     ServerSocket servidor;
     Socket sc;
 
+    public void serverConnectorJava() {
+        try {
+            servidor = new ServerSocket(PUERTO);
+            System.out.println("Servidor Iniciado");
 
+            while (true) {
+                sc = servidor.accept();
+                System.out.println("Cliente conectado");
+
+                in = new DataInputStream(sc.getInputStream());
+
+                out = new DataOutputStream(sc.getOutputStream());
+
+                String massage = in.readUTF();
+                System.out.println(massage);
+
+                ///respuesta del servidor
+                out.writeUTF("Hello word! massage from the Server✈");
+
+                //cerra el cliente, No el servidor
+                sc.close();
+                System.out.println("Cliente desconectado");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ServerJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
