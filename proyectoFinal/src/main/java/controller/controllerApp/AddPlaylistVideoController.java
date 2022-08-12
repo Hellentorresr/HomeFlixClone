9fe3986ef5979cb216a2ea7620fc943093a2bc84
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-12 15:24:50 -0600

    Creacion de la clase AddPlaylistVideoController y sus atributos

---
+++ proyectoFinal/src/main/java/controller/controllerApp/AddPlaylistVideoController.java
@@ -0,0 +1,30 @@
+/**
+ * @autor por Hellen Torres
+ * @FechaCreacion 12/08/2022
+ * @Ultima_Modificacion 12//08/2022
+ * @por Hellen torres
+ */
+package controller.controllerApp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Creacion de la clase AddPlaylistVideoController
+ */
+public class AddPlaylistVideoController {
+    /**
+     * Atributos de la clase AddPlaylistVideoController
+     */
+
+    @FXML
+    private Button btnSend;
+    @FXML
+    private TextField txtNameList;
+    @FXML
+    private TextField txtTemaList;
+
+
+
+}
