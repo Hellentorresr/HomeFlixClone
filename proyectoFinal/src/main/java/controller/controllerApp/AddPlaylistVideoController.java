6a495c4de3df7c54c5c5342076170568e6737e5b
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-15 15:19:43 -0600

    se crea el metodo addRegistration

--- proyectoFinal/src/main/java/controller/controllerApp/AddPlaylistVideoController.java
+++ proyectoFinal/src/main/java/controller/controllerApp/AddPlaylistVideoController.java
@@ -6,15 +6,23 @@
  */
 package controller.controllerApp;
 
+import controller.dao.DAOPlayListVideos;
+import controller.dao.PlaylistVideoDAOImplement;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.PlaylistVideos;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  * Creacion de la clase AddPlaylistVideoController
  */
 public class AddPlaylistVideoController {
+    public Button btnRegresar;
     /**
      * Atributos de la clase AddPlaylistVideoController
      */
@@ -27,20 +35,39 @@ public class AddPlaylistVideoController {
     private TextField txtTemaList;
 
     private PlaylistVideos playlistVideos;
+    private UtilitiesImplements utilitiesImplements;
+    private DAOPlayListVideos daoPlayListVideos;
 
     /**
      * Constructor de la clase AddPlayListVideoController
      */
     public AddPlaylistVideoController() {
         this.playlistVideos = new PlaylistVideos();
+        utilitiesImplements = new UtilitiesImplements();
+        daoPlayListVideos = new PlaylistVideoDAOImplement();
+
     }
 
     /**
      * metodo addRegistration
      */
     @FXML
-    void addRegistration() {
+    void addRegistration() throws SQLException, IOException {
         String name = this.txtNameList.getText();
         String tema = this.txtTemaList.getText();
+
+        if (name.isEmpty() || tema.isEmpty()) {
+            utilitiesImplements.mostrarMensajeNegativo("Favor llenar los campos en blanco");
+        } else {
+            float duration =0;
+            LocalDate dateNow = LocalDate.now();
+            daoPlayListVideos.insert(name,duration,tema,dateNow);
+            utilitiesImplements.mostrarMensajePositivo("Playlist Creada correctamente");
+            regresar();
+        }
+    }
+
+    public void regresar() throws IOException {
+        utilitiesImplements.pathInterfazGrafica("DentroDeLaApp.fxml",btnRegresar);
     }
 }
