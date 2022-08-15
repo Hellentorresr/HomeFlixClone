58fe68afac595ed03ff4d143fe4cbb742ac1f4c1
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-15 15:19:09 -0600

    se sobre escribe metodo insert

--- proyectoFinal/src/main/java/controller/dao/DAOPlayListVideos.java
+++ proyectoFinal/src/main/java/controller/dao/DAOPlayListVideos.java
@@ -2,5 +2,9 @@ package controller.dao;
 
 import model.PlaylistVideos;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public interface DAOPlayListVideos extends DAO<PlaylistVideos> {
+    void insert(String namePlaylist, float totalPlayListDurationTime, String tema, LocalDate creationDate) throws SQLException;
 }
