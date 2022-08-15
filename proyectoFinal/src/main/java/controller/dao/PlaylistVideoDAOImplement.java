5b9ae866744d4124754d71d9477daa4dad10b17e
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-15 15:54:03 -0600

    se modifica metodo get

--- proyectoFinal/src/main/java/controller/dao/PlaylistVideoDAOImplement.java
+++ proyectoFinal/src/main/java/controller/dao/PlaylistVideoDAOImplement.java
@@ -5,14 +5,42 @@ import model.PlaylistVideos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PlaylistVideoDAOImplement implements DAOPlayListVideos{
+public class PlaylistVideoDAOImplement implements DAOPlayListVideos {
     @Override
     public PlaylistVideos get(int indiceConteo) throws SQLException {
-        return null;
+        PlaylistVideos playlistVideos = null;
+        Connection daoConnection = Connexion.getConnection();
+        //aqui se especifican el nombre de cada columna de la tablavideo de mi base de datos
+        String sql = "SELECT id, namePlaylist, totalPlayListDurationTime, tema, creationDate, videosList FROM playlisttable WHERE id = ?";
+        PreparedStatement ps = daoConnection.prepareStatement(sql);
+        ps.setInt(1, indiceConteo);//primer parametro
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {//verificamos si tengo un record
+            //aqui obteniendo el contenido de las columnas de la base de datos del video encontrado
+            int oid = rs.getInt("id");// guardando en oid el índice o conteo del video si lo encontró
+            String nombreVideo = rs.getString("namePlaylist");
+            float duration = rs.getFloat("totalPlayListDurationTime");
+            String tema = rs.getString("tema");
+
+            String fechaString = rs.getString("creationDate");
+            DateTimeFormatter JEFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate creationDate = LocalDate.parse(fechaString, JEFormatter);
+            int videosList = rs.getInt("videosList");
+            ArrayList<Integer> videos = new ArrayList<>();
+            videos.add(videosList);
+
+            playlistVideos = new PlaylistVideos(nombreVideo, duration, tema, creationDate, oid, videos);
+        }
+        return playlistVideos;
+
     }
 
     @Override
@@ -32,7 +60,7 @@ public class PlaylistVideoDAOImplement implements DAOPlayListVideos{
 
     @Override
     public void insert(String namePlaylist, float totalPlayListDurationTime, String tema, LocalDate creationDate) throws SQLException {
-        PlaylistVideos playlistVideos1 = new PlaylistVideos(namePlaylist,totalPlayListDurationTime,tema,creationDate);
+        PlaylistVideos playlistVideos1 = new PlaylistVideos(namePlaylist, totalPlayListDurationTime, tema, creationDate);
         Connection connection = Connexion.getConnection();
         String sql = "INSERT INTO playlisttable (namePlaylist, totalPlayListDurationTime, tema, creationDate) VAlUES (?,?,?,?)";
         PreparedStatement ps = connection.prepareStatement(sql);
