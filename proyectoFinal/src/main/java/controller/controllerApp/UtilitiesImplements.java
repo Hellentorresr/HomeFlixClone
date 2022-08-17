2781c85a68fe6c101afb047acf9ab4e669487498
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-17 13:42:05 -0600

    Se edita metodo verificarSiExistePlayList

--- proyectoFinal/src/main/java/controller/controllerApp/UtilitiesImplements.java
+++ proyectoFinal/src/main/java/controller/controllerApp/UtilitiesImplements.java
@@ -110,6 +110,7 @@ public class UtilitiesImplements extends UtilitiesAbstract {
 
     /**
      * Metodo para obtener todas las listas de reproduccion registradas
+     *
      * @return retorna un arraylist con listas de reproduccion registradas
      * @throws SQLException genera una exception si no hay communication con la bae de datos
      */
@@ -126,14 +127,22 @@ public class UtilitiesImplements extends UtilitiesAbstract {
 
     /**
      * Metodo para verificar si una lista de reproduccion existe
+     *
      * @param id recibe por parametro un entero para hacer la busqueda
      * @return retorna true si encuentra una lista con ese id o false si no existe
      * @throws SQLException genera una exception si no hay communication con la bae de datos
      */
-    public boolean verificarSiExistePlayList(int id) throws SQLException {
+    public boolean verificarSiExistePlayList(int id, Video v) throws SQLException {
         PlaylistVideos f;
         f = daoPlayListVideos.get(id);
-        return allPlaylist().contains(f);
+        if (allPlaylist().contains(f)) {
+            f.agregarVideo(v);
+            System.out.println(f);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
 
