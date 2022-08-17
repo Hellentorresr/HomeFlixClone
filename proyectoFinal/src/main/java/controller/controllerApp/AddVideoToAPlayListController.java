4917455abff24ad594ac259d0e4529fa465e9aaa
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-17 11:06:17 -0600

    Se agrega metodo regresar

--- proyectoFinal/src/main/java/controller/controllerApp/AddVideoToAPlayListController.java
+++ proyectoFinal/src/main/java/controller/controllerApp/AddVideoToAPlayListController.java
@@ -1,14 +1,19 @@
 package controller.controllerApp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import model.PlaylistVideos;
+
+import java.io.IOException;
 
 /**
  * Clase AddVideoToAPlayListController
  */
 public class AddVideoToAPlayListController {
+    UtilitiesImplements utilitiesImplements;
     /**
      * atributos de la clase
      * AddVideoToAPlayListController
@@ -18,11 +23,26 @@ public class AddVideoToAPlayListController {
     @FXML
     private Button btnRegresar;
     @FXML
-    private TableColumn<?, ?> idLista;
+    private TableColumn<PlaylistVideos, Integer> idLista;
     @FXML
-    private TableColumn<?, ?> nombreLista;
+    private TableColumn<PlaylistVideos, String> nombreLista;
     @FXML
     private TextField inputTextF;
 
+    public AddVideoToAPlayListController() {
+        this.utilitiesImplements = new UtilitiesImplements();
+    }
 
+    /**
+     * Metodo para el registro del video
+     */
+    public void enviar() {
+    }
+
+    /**
+     * Metodo para el regreso
+     */
+    public void regresar() throws IOException {
+        utilitiesImplements.pathInterfazGrafica("Home.fxml",btnRegresar);
+    }
 }
