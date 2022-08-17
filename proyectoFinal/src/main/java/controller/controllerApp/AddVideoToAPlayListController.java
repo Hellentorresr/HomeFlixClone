550e7272e28adc6a1251e231d6d9f9ed48544913
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-17 11:39:11 -0600

    Se agrega metodo cargarDatos y se implementa metodo initialize

--- proyectoFinal/src/main/java/controller/controllerApp/AddVideoToAPlayListController.java
+++ proyectoFinal/src/main/java/controller/controllerApp/AddVideoToAPlayListController.java
@@ -1,19 +1,29 @@
 package controller.controllerApp;
 
-import javafx.event.ActionEvent;
+
+import controller.dao.PlaylistVideoDAOImplement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.PlaylistVideos;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * Clase AddVideoToAPlayListController
  */
-public class AddVideoToAPlayListController {
+public class AddVideoToAPlayListController implements Initializable {
     UtilitiesImplements utilitiesImplements;
+    PlaylistVideoDAOImplement playlistVideoDAOImplement;
     /**
      * atributos de la clase
      * AddVideoToAPlayListController
@@ -27,10 +37,16 @@ public class AddVideoToAPlayListController {
     @FXML
     private TableColumn<PlaylistVideos, String> nombreLista;
     @FXML
+    private TableView<PlaylistVideos> tabla;
+
+    private ObservableList<PlaylistVideos> playlistVideosOb;
+    @FXML
     private TextField inputTextF;
 
     public AddVideoToAPlayListController() {
         this.utilitiesImplements = new UtilitiesImplements();
+        playlistVideosOb = FXCollections.observableArrayList();
+        playlistVideoDAOImplement = new PlaylistVideoDAOImplement();
     }
 
     /**
@@ -42,7 +58,25 @@ public class AddVideoToAPlayListController {
     /**
      * Metodo para el regreso
      */
+    @FXML
     public void regresar() throws IOException {
-        utilitiesImplements.pathInterfazGrafica("Home.fxml",btnRegresar);
+        utilitiesImplements.pathInterfazGrafica("Home.fxml", btnRegresar);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            cargarDatos();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void cargarDatos() throws SQLException {
+        playlistVideosOb.addAll(playlistVideoDAOImplement.getALL());
+        this.idLista.setCellValueFactory(new PropertyValueFactory<>("id"));
+        this.nombreLista.setCellValueFactory(new PropertyValueFactory<>("namePlaylist"));
+        tabla.setItems(playlistVideosOb);
     }
 }
