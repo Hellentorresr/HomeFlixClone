5242eefb55701500cc15106660d97f435d8cedba
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-17 10:54:27 -0600

    Se crea clase AddVideoToAPlayListController con sus atributos

---
+++ proyectoFinal/src/main/java/controller/controllerApp/AddVideoToAPlayListController.java
@@ -0,0 +1,28 @@
+package controller.controllerApp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+/**
+ * Clase AddVideoToAPlayListController
+ */
+public class AddVideoToAPlayListController {
+    /**
+     * atributos de la clase
+     * AddVideoToAPlayListController
+     */
+    @FXML
+    private Button btnRegistro;
+    @FXML
+    private Button btnRegresar;
+    @FXML
+    private TableColumn<?, ?> idLista;
+    @FXML
+    private TableColumn<?, ?> nombreLista;
+    @FXML
+    private TextField inputTextF;
+
+
+}
