2217ee5652d1960ed89ee8f8be823d6413b385e2
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-08-17 18:36:26 -0600

    Se agregan atributos daoPlayListVideos, playlistVideosOb y playlistVideosOb

--- proyectoFinal/src/main/java/controller/controllerApp/CRUDPlayListController.java
+++ proyectoFinal/src/main/java/controller/controllerApp/CRUDPlayListController.java
@@ -1,50 +1,63 @@
 package controller.controllerApp;
 
-import javafx.event.ActionEvent;
+import controller.dao.DAOPlayListVideos;
+import controller.dao.PlaylistVideoDAOImplement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.PlaylistVideos;
 
-public class CRUDPlayListController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+/**
+ * Clase CRUDPlayListController
+ */
+public class CRUDPlayListController implements Initializable {
+    /**
+     * Atributos de la clase CRUDPlayListController
+     */
+    UtilitiesImplements utilitiesImplements;
+    DAOPlayListVideos daoPlayListVideos;
     @FXML
     private Button btnEditar;
-
     @FXML
     private Button btnEliminar;
-
     @FXML
     private Button btnRegresar;
-
     @FXML
-    private TableColumn<?, ?> columnDate;
-
+    private TableColumn<PlaylistVideos, String> columnDate;
     @FXML
-    private TableColumn<?, ?> columnId;
-
+    private TableColumn<PlaylistVideos, Integer> columnId;
     @FXML
-    private TableColumn<?, ?> columnNombre;
-
+    private TableColumn<PlaylistVideos, String> columnNombre;
     @FXML
-    private TableColumn<?, ?> columnTema;
-
+    private TableColumn<PlaylistVideos, String> columnTema;
     @FXML
     private TextField inputIdEditar;
-
     @FXML
     private TextField inputIdEliminar;
-
     @FXML
-    private TableView<?> tabla;
-
+    private TableView<PlaylistVideos> tabla;
     @FXML
     private TextField txtNombre;
-
     @FXML
     private TextField txtTema;
+    private ObservableList<PlaylistVideos> playlistVideosOb;
 
+    public CRUDPlayListController() {
+        this.playlistVideosOb = new UtilitiesImplements();
+        playlistVideosOb = FXCollections.observableArrayList();
+        daoPlayListVideos = new PlaylistVideoDAOImplement();
+    }
 
 
 }
